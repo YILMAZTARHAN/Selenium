0d5b0d6f1d7bfd6765faab5ebdf368044a117f41
Yılmaz Tarhan <aslankral3325@gmail.com>
2022-01-18 00:40:15 +0300

    2 Gün

--- src/test/java/day02_driverMethods/C08_ImplicitlyWait.java
+++ src/test/java/day02_driverMethods/C08_ImplicitlyWait.java
@@ -11,22 +11,17 @@ public class C08_ImplicitlyWait {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        // implicitly wait testimiz boyunca sayfa acilmasi ve element bulunmasi konusunda driver'in
+        // bekleyecegi maximum sureyi belirler.
+        // Bu sure icerisinde gorevini bitirir bitirmez, bir sonraki adima gecer
+        // Bu sure icerisinde gorevini bitiremezse o zaman hata verir
 
-
-
-
-
-
-
-
+        // belirlenen maximum sure Thread.sleep'deki gibi mutlak bekleme suresi degildir
 
 
         driver.get("https://techproeducation.com");
-        //Thread.sleep(5000);
+        // Thread.sleep(5000);
 
         driver.close();
     }
-
-
-
 }
