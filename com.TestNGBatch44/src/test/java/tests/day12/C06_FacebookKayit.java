a6cac000697eb434d8ee6db0714d2d3c70e618ff
Yılmaz Tarhan <aslankral3325@gmail.com>
2022-02-01 00:28:49 +0300

    14.Gun

--- com.TestNGBatch44/src/test/java/tests/day12/C06_FacebookKayit.java
+++ com.TestNGBatch44/src/test/java/tests/day12/C06_FacebookKayit.java
@@ -17,7 +17,7 @@ public class C06_FacebookKayit extends TestBase {
         //1- https://www.facebook.com adresine gidelim
         driver.get("https://www.facebook.com");
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//button[text()='Allow All Cookies']")).click();
+
         //2- Yeni hesap olustur butonuna basalim
         driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
 
---
+++ com.TestNGBatch44/src/test/java/tests/day14/C04_Screenshot.java
@@ -0,0 +1,8 @@
+package tests.day14;
+
+import utilities.TestBase;
+
+public class C04_Screenshot extends TestBase {
+
+
+}
