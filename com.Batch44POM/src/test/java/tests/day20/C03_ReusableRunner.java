3db90b6ec6d8117818d98dfa358ef43f7e9d9d68
Yılmaz Tarhan <aslankral3325@gmail.com>
2022-02-08 00:31:16 +0300

    20.Gun

---
+++ com.Batch44POM/src/test/java/tests/day20/C03_ReusableRunner.java
@@ -0,0 +1,23 @@
+package tests.day20;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class C03_ReusableRunner {
+
+
+    @Test
+    public void test01(){
+
+        // Ülkeler excelindeki sayfa1'de
+        //11.ndex'deki satırın,2.indexdeki hücresinin Azerbaycan olduğunu test edin
+
+        String path="src/test/java/resources/ulkeler.xlsx";
+        String expectedData="Azerbaycan";
+        String actualData=ReusableMethods.hucreGetir(path,"sayfa1",11,2).toString();
+
+        Assert.assertEquals(actualData,expectedData);
+
+
+    }
+}
